54fa6a5525bb7fac5b8f10bb8996defa1b71057d
aniskoff <cme412@mail.ru>
2021-03-25 22:31:18 +0300

    added statistical recommendation experiments

--- sklearn_hp_analisys/util/file_util.py
+++ sklearn_hp_analisys/util/file_util.py
@@ -1,5 +1,5 @@
 # Copyright (c) Aniskov N.
-
+import json
 import os
 from typing import List
 
@@ -17,3 +17,9 @@ def get_all_src_files_from_dir(dirname: str) -> List[str]:
             sample_filename = os.path.join(dirname, entry)
             result.append(sample_filename)
     return result
+
+
+def write_json_to_file(result, out_filename):
+
+    with open(out_filename, 'w') as out_f:
+        json.dump(result, out_f)
